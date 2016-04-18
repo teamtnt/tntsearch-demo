a83d28f1f08b33644191423068fa1d22f7330aad
Nenad Ticaric <nticaric@gmail.com>
2016-04-18 21:11:12 +0200

    query expansion

--- app/QueryExpansion.php
+++ app/QueryExpansion.php
@@ -5,8 +5,19 @@ namespace App;
 class QueryExpansion
 {
     public static $query = [
-        'house'   => 'house hugh',
-        'friends' => 'friends aniston',
+        'house'          => 'house hugh',
+        'friends'        => 'friends aniston',
+        'game of'        => 'game of thrones',
+        'breaking'       => 'breaking bad',
+        'walker'         => 'walker texas',
+        'nanny'          => 'nanny fran',
+        'how'            => 'how i',
+        'the big bang'   => 'the big bang theory',
+        'the big'        => 'the big bang theory',
+        'malcolm'        => 'malcolm in the middle',
+        'two and a half' => 'two and a half men',
+        'two and a'      => 'two and a half men',
+        'two and'        => 'two and a half men',
     ];
 
     public static function expand($query)
